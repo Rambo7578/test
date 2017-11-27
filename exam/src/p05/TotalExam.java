315d282695be7c8a6cf28086ae47c1878b41581d
k1hp9052 <k1hp9052@gmail.com>
2017-11-27 17:44:39 +0900

    후아

--- exam/src/p05/TotalExam.java
+++ exam/src/p05/TotalExam.java
@@ -10,12 +10,23 @@ public class TotalExam {
 		public static void main(String[]args) {
 		
 		TotalExam te = new TotalExam();
-			if(te.a==1|| te.a==2|| te.a==3)
+			if(te.a==1|| te.a==2|| te.a==3) {
 				System.out.print("a가 0이 아니네요");
-			else if(te.a==0 && te.a==1);
+			}else if(te.a==0);{
 			System.out.print("a가 0이 네요");
-		}				
+		}	
+			System.out.print(Integer.parseInt("167")==167);//Interger.parseInt는 문자를 숫자로 바꿔준다.
+			System.out.print(Double.parseDouble("3.14"));
+		
+		// String랑 String[]스트링 배열은 전혀 다른다.
+			
+			
+			String[] strarr=new String[3];
+			strarr[0]="123";
+			strarr[1]="123";
+			strarr[2]="123";
+		
+		}
 }
-
 
 
